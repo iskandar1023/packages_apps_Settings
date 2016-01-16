22b24a00a1694d90e164ac621843298d0b81fbad
beanstown106 <nbenis106@gmail.com>
2016-01-16 16:38:23 -0500

    Fingerprint authentication vibration [2/2]

--- src/com/android/settings/velvet/CustomizationSettings.java
+++ src/com/android/settings/velvet/CustomizationSettings.java
@@ -2,19 +2,31 @@ package com.android.settings.velvet;
 
 import android.os.Bundle;
 
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.preference.SystemSettingSwitchPreference;
+
 
 public class CustomizationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String MISC_CAT = "misc";
+
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+
+    private FingerprintManager mFingerprintManager;
+    private SystemSettingSwitchPreference mFingerprintVib;
 
     @Override
     protected int getMetricsCategory() {
@@ -26,6 +38,14 @@ public class CustomizationSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.velvet_customization_settings);
+
+        PreferenceCategory miscCategory = (PreferenceCategory) findPreference(MISC_CAT);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            miscCategory.removePreference(mFingerprintVib);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
