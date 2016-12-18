2bb241889bbbee85b0d5b5605c4eb2de3551898b
Ivan Iskandar <iiiiskandar14@gmail.com>
2016-12-18 07:18:55 +0100

    Move Customization settings to "Device" category
    and add tile summary

--- src/com/android/settings/velvet/CustomizationSettings.java
+++ src/com/android/settings/velvet/CustomizationSettings.java
@@ -1,5 +1,6 @@
 package com.android.settings.velvet;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
+import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -51,4 +53,31 @@ public class CustomizationSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         return true;
     }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                mSummaryLoader.setSummary(this, mContext.getString(R.string.customization_settings_summary));
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
 }
