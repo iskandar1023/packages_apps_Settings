7fd0c9011ceaf828a5cb2f92772649bf1cdcaffb
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-01-12 14:07:40 +0100

    Add substratum settings summary

--- src/com/android/settings/velvet/SubstratumLaunch.java
+++ src/com/android/settings/velvet/SubstratumLaunch.java
@@ -1,11 +1,14 @@
 package com.android.settings.velvet;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
+import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -28,4 +31,31 @@ public class SubstratumLaunch extends SettingsPreferenceFragment {
         startActivity(i);
         finish();
     }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                mSummaryLoader.setSummary(this, mContext.getString(R.string.substratum_settings_summary));
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
 }
